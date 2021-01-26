c83119109ee4c8b8079b6ebf826623663191fe2e
John Dovey <dovey.john@gmail.com>
2021-01-26 12:37:14 -0500

    Cleanup

--- cpp/PercCalc.cpp
+++ cpp/PercCalc.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-
 		char FirstNum[20]; //First Number
-			char SecondNum[20]; //Second Number
-				int MyAns = 0;//My Answer
-					int iFirstNum = 0;
-						int iSecondNum = 0;
+		char SecondNum[20]; //Second Number
+		int MyAns = 0;//My Answer
+		int iFirstNum = 0;
+		int iSecondNum = 0;
 
+		cout << "Enter First Number and hit ENTER " <<endl;
+		cin >> FirstNum;
+		iFirstNum = stoi(FirstNum);
+		cout << "Enter Second Number and hit ENTER "<<endl;
+		cin >> SecondNum;
+		iSecondNum = stoi(SecondNum);
 
-							cout << "Enter First Number and hit ENTER " <<endl;
-								cin >> FirstNum;
-									iFirstNum = stoi(FirstNum);
-										cout << "Enter Second Number and hit ENTER "<<endl;
-											cin >> SecondNum;
-												iSecondNum = stoi(SecondNum);
+		MyAns = (iFirstNum * 100) / iSecondNum;
 
-													MyAns = (iFirstNum * 100) / iSecondNum;
+		cout << "The Percentage is "<< MyAns <<"%" <<endl;
 
-														cout << "The Percentage is "<< MyAns <<"%" <<endl;
-
-														    return 0;
+			return 0;
 }
 
