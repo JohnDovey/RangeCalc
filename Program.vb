904ef3ac1d88299632222099c03deba466492c59
JohnDovey <dovey.john@gmail.com>
2021-01-26 17:42:20 -0500

    Error checking for input fields

--- Program.vb
+++ Program.vb
@@ -2,26 +2,29 @@ Imports System
 
 Module Program
     Sub Main()
-		Dim Bearing1 As Integer ' bearing from left ref point to target
-		Dim Bearing2 As Integer ' bearing from right ref point to target
-		Dim RefDistance As Integer ' distance between two ref points _ use %u for unsigned number
-		Dim RangeToTarget As Double ' Calculated range
-		Dim tmpAngle As Integer
+		Dim Bearing1 As Integer = 0' bearing from left ref point to target
+		Dim Bearing2 As Integer = 0' bearing from right ref point to target
+		Dim RefDistance As Integer = 0' distance between two ref points _ use %u for unsigned number
+		Dim RangeToTarget As Double = 0' Calculated range
+		Dim tmpAngle As Integer = 0
 		Dim OutTxt(2) As String
-		OutTxt(0) = "+------------------------------------------------------------------+"
+		Dim ProgVer as String = "1.0.12"
+		Dim inputStr as String = ""
+		OutTxt(0) = "+------------------------------[{0,6}]----------------------------+"
 		OutTxt(1) = "| Range Calculator - by John Dovey <dovey.john@gmail.com>          |"
 		OutTxt(2) = "| View the code on GitHub [https://github.com/JohnDovey/RangeCalc] |"
-		Console.WriteLine(OutTxt(0))
+		Console.WriteLine(OutTxt(0), ProgVer)
 		Console.WriteLine(OutTxt(1))
 		Console.WriteLine(OutTxt(2))
-		Console.WriteLine(OutTxt(0))
+		Console.WriteLine(OutTxt(0), ProgVer)
 
 
 		Console.WriteLine(vbLf & vbLf & " Enter the values as prompted")
 
 		' Bearing One
 		Console.Write("Bearing from Ref One to Target: ")
-		Bearing1 = Console.ReadLine
+		inputStr=Console.ReadLine
+		Bearing1 = val(inputStr)
 		Console.WriteLine(vbTab & "({0} degrees bearing Ref 1 to target) ", Bearing1)
 
 		If Bearing1 > 360 Then
@@ -35,7 +38,8 @@ Module Program
 		End If
 		' Bearing Two
 		Console.Write("Bearing from Ref Two to Target: ")
-		Bearing2 = Console.ReadLine
+		inputStr = Console.ReadLine
+		Bearing2 = val(inputStr)
 
 		Console.WriteLine(vbTab & "({0} degrees bearing Ref 2 to target) ", Bearing2)
 
@@ -50,7 +54,8 @@ Module Program
 
 		' Seperation Distance
 		Console.Write("Distance between Ref One and Ref Two: ")
-		RefDistance = Console.ReadLine
+		inputStr = Console.ReadLine
+		RefDistance = val(inputStr)
 		Console.WriteLine(vbTab & "({0} meters between reference points) ", RefDistance)
 
 		' d = (Tan (90 - (A -B))) x Ref
